0ce7135f3175dfd86c70461487432a8ce1b8797b
Henrique_Veras <theveras1@gmail.com>
2023-05-01 19:02:08 -0300

    Validation of reused username

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,14 @@ const UserService = require('../services/userService');
 
 const createUser = async(req, res) => {
     const body = req.body;
-    console.log(body);
     try{
         const newUser = await UserService.createUser(body);
-        if (newUser.message) {
+        if (newUser.message === 'User already in use.') {
+            return res.status(400).json({message: newUser.message});
+        } else {
             return res.status(201).json({message: newUser.message, user: newUser.user});
         }
+
     } catch(err) {
         console.log(err);
         return res.status(500).json({error: err});
--- services/userService.js
+++ services/userService.js
@@ -4,13 +4,28 @@ require('dotenv').config();
 
 const secret = process.env.JWT_SECRET;
 
+
+const validateNewUser = async(username) => {
+    const findUsername = await User.findOne({ where: { username } });
+    if (findUsername) {
+        return { errMessage: 'User already in use.' };
+    } else {
+        return { validUsername: true };
+    }
+};
+
 const createUser = async(body) => {
-    const {username, password } = body;
-    const hashPass = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ username, password: hashPass });
-    return {message: 'User Created!', user: newUser};
+    const { username, password } = body;
+    const validateUser = await validateNewUser(username);
+    if (validateUser.errMessage) {
+        return {message: validateUser.errMessage};
+    } else {
+        const hashPass = await bcrypt.hash(password, 10);
+        const newUser = await User.create({ username, password: hashPass });
+        return {message: 'User Created!', user: newUser};
+    }
 }
 
 module.exports = {
-    createUser
+    createUser,
 };
